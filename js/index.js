26e5c435fd32804e5c840476ff884bdf520e3f65
RomaP28 <posohovrom@icloud.com>
2022-02-08 16:41:40 +0200

    calc

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@
 $(function () {
   //--------------------клик по кнопке с цифрой-----------------------------------------------------------------------
   $('.num-button').click(function () {
-    // debugger
     if ($('#display').val() == '0' || $('#display').data('condition') == true) {
       $('#display').data('condition', false);
       $('#display').val('');
@@ -11,15 +10,18 @@ $(function () {
   });
   //--------------------клик по одной из функциональных кнопок-----------------------------------------------------------------
   $('.function-button').click(function () {
-    $('#display').data('valueOne', $('#display').val());
     $('#display').data('operator', $(this).text());
     if (/(-|\/|\+|\*)$/.test($('#display').val())) {
+      $('#display').data('valueOne', $('#display').val());
       let str = $('#display').val();
       str = str.replace(/(-|\/|\+|\*)$/, $(this).text());
       $('#display').val(str);
     } else {
       if (/(-|\/|\+|\*)/.test($('#display').val())) {
-        $('#display').val($('#display').val())
+        let valueB = $('#display').val().split('').splice($('#display').data('valueOne').length + 1, $('#display').val().length).join('');
+        $('#display').data('valueTwo', valueB);
+        $('.equals-button').click();
+        // $('#display').val($('#display').val())
       } else {
         $('#display').val($('#display').val() + $(this).text());
       }
@@ -32,7 +34,9 @@ $(function () {
       $('#display').val($('#display').val());
     } else {
       let valueA = $('#display').data('valueOne');
-      let valueB = $('#display').val().split('').splice($('#display').data('valueOne').length + 1, $('#display').val().length).join('');
+      if ($('#display').data('valueTwo') == '') {
+        let valueB = $('#display').val().split('').splice($('#display').data('valueOne').length + 1, $('#display').val().length).join('');
+      }
       let operator = $('#display').data('operator');
       let result;
       switch (operator) {
@@ -52,6 +56,7 @@ $(function () {
         $('#display').val("ERROR");
       }
       $('#display').data('condition', true);
+      // resetCalculator();
     }
   });
   //-------------------клик по копке очистки экрана--------------------------------------------------------------------
@@ -62,6 +67,7 @@ $(function () {
   function resetCalculator() {
     $('#display').val('0');
     $('#display').data('valueOne', '');
+    $('#display').data('valueTwo', '');
     $('#display').data('operator', '');
     $('#display').data('condition', false);
   }
